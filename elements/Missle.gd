8c5bc7b863513814af741dbed8158fc6367c4d8b
Alex Haynes <aheinous@gmail.com>
2019-05-01 15:46:39 -0700

    Add missile explosion sound

--- elements/Missle.gd
+++ elements/Missle.gd
@@ -1,15 +1,16 @@
 extends Area2D
 
-
-
 export var speed : float = 700
 
-
 onready var animatedSprite = $'AnimatedSprite'
+onready var explosionSound = $'ExplosionSound2D'
 
 var velocity = null
-
 var exploding = false
+
+
+var sndDone = false
+var animationDone = false
 
 func _ready():
 	velocity = Vector2(0, -speed).rotated(deg2rad(self.rotation_degrees))
@@ -26,16 +27,28 @@ func _on_Missle_body_entered(body):
 		print('hit player')
 		body.collideWith(self)
 	animatedSprite.play('explode')
+	explosionSound.play()
 	exploding = true
 	velocity = Vector2()
 
 
+func maybeFree():
+	if sndDone and animationDone:
+		queue_free()
+
 
 func _on_AnimatedSprite_animation_finished():
 	if exploding:
-		queue_free()
+		animationDone = true
+		animatedSprite.queue_free()
+		maybeFree()
 
 
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_ExplosionSound2D_finished():
+	sndDone = true
+	maybeFree()
