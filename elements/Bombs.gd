47b65b87f243b8edff8d81362e36688345510eb7
Alex Haynes <aheinous@gmail.com>
2019-05-15 21:44:41 -0700

    Improve bomb collision detection

--- elements/Bombs.gd
+++ elements/Bombs.gd
@@ -1,40 +1,32 @@
 extends TileMap
 
-
 const explosionScene = preload('res://elements/Explosion.tscn')
-
-
-
-func _ready():
-	pass
-
 
 func onPlayerHit(player, collision):
 
 	var tilePos = world_to_map(collision.position - collision.normal*(cell_size/2))
 	var celln = get_cellv(tilePos)
+#	var tryNum = 0
+
 	if celln == -1:
+		tilePos = world_to_map(collision.position - collision.normal.rotated(deg2rad(10))*(cell_size/2))
+		celln = get_cellv(tilePos)
+#		tryNum = 1
+
+	if celln == -1:
+		tilePos = world_to_map(collision.position - collision.normal.rotated(deg2rad(-10))*(cell_size/2))
+		celln = get_cellv(tilePos)
+#		tryNum = 2
+
+#	print('Bomb Hit: %s %s %s, %s, %d' % [tilePos, collision.position, collision.normal, celln, tryNum])
+
+	if celln == -1:
+		print('false bomb hit')
 		return false
+
 	set_cellv(tilePos, -1)
-#	update_dirty_quadrants()
-	print('bomb hit by player: ', collision, ', ', tilePos, ', ', celln)
 
 	var explosion = explosionScene.instance()
 	explosion.position = map_to_world(tilePos) + cell_size / 2
 	get_tree().get_current_scene().add_child(explosion)
 	return true
-
-
-
-
-
-
-
-
-
-
-
-
-
-	# print('tilePos: ', tilePos)
-	# print('normal: ', collision.normal)
