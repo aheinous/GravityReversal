bbeb04ecb52076cd9bedc3f310d99396d8529610
Alex Haynes <aheinous@gmail.com>
2019-06-07 00:55:35 -0700

    Change gem particle emissions.

--- elements/gems/BlueGem.gd
+++ elements/gems/BlueGem.gd
@@ -9,6 +9,21 @@ onready var sndfx = $AudioStreamPlayer
 export var color = 'blue'
 
 
+
+func _ready():
+	match color:
+		'blue':
+			particles.modulate = Color(0, 0.3, 1)
+		'red':
+			particles.modulate = Color(1, 0, 0)
+		'green':
+			particles.modulate = Color(0, 1, 0)
+		'yellow':
+			particles.modulate = Color(1, 1, 0)
+		_:
+			assert(false)
+
+
 func registerGem():
 	sprite.visible = false
 	get_tree().get_current_scene().onGem(color)
