84ee0e1ca6d092ec7e353443d1d6d6ad8472ad7e
Alex Haynes <aheinous@gmail.com>
2019-06-10 16:48:06 -0700

    Add separate pause menu

---
+++ menus/pauseMenu.gd
@@ -0,0 +1,53 @@
+extends CanvasLayer
+
+
+
+const confirmPkdScn = preload('res://menus/ConfirmDialog.tscn')
+
+
+func _on_continueButton_pressed():
+	get_tree().get_current_scene().togglePause()
+
+
+func onQuitConfirmed():
+	LevelTransitions.quitLevel()
+	get_tree().paused = false
+
+
+func _on_quitButton_pressed():
+	var confirm = confirmPkdScn.instance()
+	add_child(confirm)
+	confirm.setup("Quit to main menu?",
+				"Yes", self, "onQuitConfirmed",
+				"No")
+
+func _on_optionsButton_pressed():
+	var optionsPkdScn = preload("res://menus/Options.tscn")
+	self.add_child(optionsPkdScn.instance())
+
+
+func _on_restartButton_pressed():
+	var confirm = confirmPkdScn.instance()
+	add_child(confirm)
+	confirm.setup("Restart level?",
+				"Yes", self, "onRestartConfirmed",
+				"No")
+
+func onRestartConfirmed():
+	LevelTransitions.restartAtBeginning()
+	get_tree().paused = false
+
+
+
+func _on_loadChkptButton_pressed():
+	var confirm = confirmPkdScn.instance()
+	add_child(confirm)
+	confirm.setup("Load last checkpoint?",
+				"Yes", self, "onLoadChkptConfirmed",
+				"No")
+
+
+func onLoadChkptConfirmed():
+	LevelTransitions.restartAtCheckpointOrBeginning()
+	get_tree().paused = false
+
