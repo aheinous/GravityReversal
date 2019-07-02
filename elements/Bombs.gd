5164a3052aa80a9b3ea1136fd324ac1d6fe79719
Alex Haynes <aheinous@gmail.com>
2019-07-02 16:30:20 -0700

    Fix bomb collisions bug due to scaling and not having collision shapes re-implemented.

--- elements/Bombs.gd
+++ elements/Bombs.gd
@@ -3,18 +3,19 @@ extends TileMap
 const explosionScene = preload('res://elements/Explosion.tscn')
 
 func onPlayerHit(player, collision):
+	var halfCellSize = scale * cell_size / 2
 
-	var tilePos = world_to_map(collision.position - collision.normal*(cell_size/2))
+	var tilePos = world_to_map( (collision.position - collision.normal*halfCellSize) / scale )
 	var celln = get_cellv(tilePos)
 #	var tryNum = 0
 
 	if celln == -1:
-		tilePos = world_to_map(collision.position - collision.normal.rotated(deg2rad(10))*(cell_size/2))
+		tilePos = world_to_map( (collision.position - collision.normal.rotated(deg2rad(10))*halfCellSize) / scale )
 		celln = get_cellv(tilePos)
 #		tryNum = 1
 
 	if celln == -1:
-		tilePos = world_to_map(collision.position - collision.normal.rotated(deg2rad(-10))*(cell_size/2))
+		tilePos = world_to_map( (collision.position - collision.normal.rotated(deg2rad(-10))*halfCellSize) / scale )
 		celln = get_cellv(tilePos)
 #		tryNum = 2
 
@@ -27,6 +28,6 @@ func onPlayerHit(player, collision):
 	set_cellv(tilePos, -1)
 
 	var explosion = explosionScene.instance()
-	explosion.position = map_to_world(tilePos) + cell_size / 2
+	explosion.position = map_to_world(tilePos)*scale + halfCellSize
 	get_tree().get_current_scene().add_child(explosion)
 	return true
